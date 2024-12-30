a2a76c3253cc7a9d8b66d0d6afb0c899f3bb5f94
alex <lapkesa@gmail.com>
2024-12-30 17:08:34 +0200

    add svg icon to navbar

--- src/components/NavBar/index.jsx
+++ src/components/NavBar/index.jsx
@@ -2,12 +2,13 @@ import React from 'react';
 import { Link } from 'react-router-dom';
 import './NavBar.css';
 import svg from '../../UI/icons/test.svg';
+import personSvg from '../../UI/icons/person.svg';
 
 const NavBar = () => {
   return (
     <div className='navbar'>
       <Link to='/' className='navbar__button'>
-        <img src={svg} alt='CardList' className='navbar__icon' />
+        <img src={personSvg} alt='CardList' className='navbar__icon' />
       </Link>
       <Link to='/page2' className='navbar__button'>
         <img src={svg} alt='Page 2' className='navbar__icon' />
