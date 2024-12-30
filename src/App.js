f5a2836e163cf2c4b7dc30e01e6bf932e77724da
alex <lapkesa@gmail.com>
2024-12-30 17:25:09 +0200

    scroll test1

--- src/App.js
+++ src/App.js
@@ -3,21 +3,14 @@ import { BrowserRouter as Router, Routes, Route } from 'react-router-dom';
 import './App.css';
 import CardList from './components/CardList';
 import NavBar from './components/NavBar';
-import Vremeno from './components/vremeno';
+import vremeno from './components/vremeno';
 
 function App() {
   useEffect(() => {
     if (window.Telegram?.WebApp) {
       const webApp = window.Telegram.WebApp;
-
-      // Сообщаем Telegram, что приложение готово
-      webApp.ready();
-
-      // Разворачиваем WebView на полный экран
-      webApp.expand();
-
-      // Отключаем автоматическое сворачивание при скролле
-      document.body.style.overflow = 'hidden';
+      webApp.expand(); // Развернуть приложение
+      document.body.style.minHeight = `${webApp.viewportHeight}px`;
     }
   }, []);
 
@@ -26,8 +19,8 @@ function App() {
       <div className='App'>
         <Routes>
           <Route path='/' element={<CardList />} />
-          <Route path='/page2' element={<Vremeno />} />
-          <Route path='/page3' element={<Vremeno />} />
+          <Route path='/page2' element={<vremeno />} />
+          <Route path='/page3' element={<vremeno />} />
         </Routes>
         <NavBar />
       </div>
